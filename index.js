52d9738ced148b47e539e6b45a06c939f58574ee
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2022-02-13 01:59:54 +0400

    added timer functionality

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const h = document.querySelector("hours");
-const m = document.querySelector("minutes");
-const s = document.querySelector("seconds");
-
 function checkTime() {
-    if(h.isInteger() && m.isInteger() && s.isInteger()) {
-        console.log("Entered numbers");
+
+    var h = document.querySelector(".hours");
+    var m = document.querySelector(".minutes");
+    var s = document.querySelector(".seconds");
+    
+
+    if (h.value=="00" && m.value=="00" && s.value=="00" ) {
+        alert("No time set!")
     } else {
-        alert('Enter Time in Integers!');
-        h.innerHTML = "00";
-        m.innerHTML = "00";
-        s.innerHTML = "00";
+        h.value=parseInt(h.value);
+        m.value=parseInt(m.value);
+        s.value=parseInt(s.value);
+
+        if(h.value=="NaN" || m.value=="NaN" || s.value=="NaN") {
+            h.value="00";
+            m.value="00";
+            s.value="00";
+            alert('Enter time in integers!');
+        } else {
+            console.log("All numbers are int!");
+            var time =s.value+(m.value*60)+(h.value*3600);
+            console.log('total time: '+time+' seconds');
+            PlayMusic(time);
+        }
+        
     }
+}
+
+
+function PlayMusic(time) {
+    1;
 }
